b0b3a8e8f5e214435a8d3f639648d55b3b9cfa6e
hejpecher Janssen <hendrik.janssen@pecher.de>
2024-02-08 12:01:23 +0000

    Links Hintergrundkarten

--- src/lib/help/Help15Datengrundlage.js
+++ src/lib/help/Help15Datengrundlage.js
@@ -109,21 +109,50 @@ const Component = () => {
               (OpenStreetMap contributors).
             </li>
             <li>
-              <strong>Luftbildkarte</strong>: (1) Kartendienst (WMS) des RVR.
+              <strong>Luftbildkarte</strong>: (1) Kartendienst (WMS) des Landes NRW.
               Datengrundlage:{" "}
               <strong>Digitale Orthophotos (DOP) des Landes NRW</strong>{" "}
               WMS-Dienst für farbige, digitale, georeferenzierte, lagegenaue,
               entzerrte Luftbilder des Landes NRW. (
               <a
                 target="_legal"
-                href="https://www.bezreg-koeln.nrw.de/brk_internet/geobasis/luftbildinformationen/aktuell/digitale_orthophotos/index.html"
+                href="https://www.bezreg-koeln.nrw.de/geobasis-nrw/produkte-und-dienste/luftbild-und-satellitenbildinformationen/aktuelle-luftbild-und-0"
               >
-                weiter Informationen
+                weitere Informationen
               </a>
-              ). (2) Kartendienste (WMS) des Regionalverbandes Ruhr (RVR).
-              Datengrundlagen: <strong>Stadtkarte 2.0</strong> und{" "}
-              <strong>Kartenschrift aus der Stadtkarte 2.0</strong>. (Details s.
-              Hintergrundkarte Stadtplan).
+              ). (2) Kartendienste (WMS) des Regionalverbandes Ruhr (RVR). 
+              Datengrundlagen: Stadtkarte 2.0 Wöchentlich in einem automatischen Prozess 
+              aktualisierte Zusammenführung des Straßennetzes der OpenStreetMap mit 
+              Amtlichen Geobasisdaten des Landes NRW aus den Fachverfahren ALKIS 
+              (Gebäude, Flächennutzungen) und ATKIS (Gewässer). © RVR und 
+              Kooperationspartner 
+              (
+              <a
+                target="_legal"
+                href="https://www.govdata.de/dl-de/by-2-0"
+              >
+                Datenlizenz Deutschland - Namensnennung - Version 2.0
+              </a>
+              ). 
+              Lizenzen der Ausgangsprodukte: 
+              (
+              <a
+                target="_legal"
+                href="https://www.govdata.de/dl-de/zero-2-0"
+              >
+                Datenlizenz Deutschland - Zero - Version 2.0
+              </a>
+              ) 
+              (Amtliche Geobasisdaten) und ODbL 
+              (
+              <a
+                target="_legal"
+                href="https://opendatacommons.org/licenses/odbl/1-0/"
+              >
+                OpenStreetMap contributors
+              </a>
+              )
+              .
             </li>
             <li>
               <strong>DTK (schwarz-weiß)</strong>: DTK Sammeldienst des Landes NRW.
@@ -140,13 +169,16 @@ const Component = () => {
               Nutzungsbedingungen: siehe{" "}
               <a
                 target="_legal"
-                href="http://www.bezreg-koeln.nrw.de/brk_internet/geobasis/lizenzbedingungen_geobasis_nrw.pdf"
+                href="https://www.bezreg-koeln.nrw.de/system/files/media/document/file/lizenzbedingungen_geobasis_nrw.pdf"
               >
                 Nutzungsbedingungen Geobasis NRW
               </a>
               , Für die DTK 250, DTK 500 gelten die Nutzungsbedingungen des BKG:
               ©{" "}
-              <a target="_legal" href="www.govdata.de/dl-de/by-2-0">
+              <a 
+              target="_legal" 
+              href="https://www.govdata.de/dl-de/by-2-0"
+              >
                 GeoBasis-DE / BKG(2020) dl-de/by-2-0
               </a>
             </li>
